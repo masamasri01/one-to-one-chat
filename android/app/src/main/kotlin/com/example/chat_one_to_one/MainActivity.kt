3b10c03033d497c88c965bc28af1537f4317b318
Masa <s11923986@stu.najah.edu>
2023-02-16 16:20:00 +0200

    commit

---
+++ android/app/src/main/kotlin/com/example/chat_one_to_one/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chat_one_to_one
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'homepage.dart';
+import 'firebase_options.dart';
+import 'package:flutterfire_ui/auth.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.indigo),
+      themeMode: ThemeMode.light,
+      home: const AuthGate(),
+    );
+  }
+}
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return MyHomePage();
+          } else
+            return SignInScreen(
+                providerConfigs: [EmailProviderConfiguration()]);
+        });
+  }
+}
